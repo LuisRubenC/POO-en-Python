76571f78a1f212044e6f04a4ce5c98f4a233d733
AnglAjc <angeladrianjuarezcorona@gmail.com>
2025-06-26 00:27:52 -0600

    Add files via upload

---
+++ Gestor_Escuela/Gestor_Cursos/src/Clase.java
@@ -0,0 +1,9 @@
+public class Clase {
+    String nombreCurso;
+    String ProfesorAsignado;
+
+    public Clase(String nombreCurso,String ProfesorAsignado) {
+        this.nombreCurso=nombreCurso;
+        this.ProfesorAsignado=ProfesorAsignado;
+    }
+}
